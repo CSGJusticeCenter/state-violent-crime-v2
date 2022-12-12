63f5e3b37f0dd3b801e7b1d7cca7f01d4292b9cf
Matt Herman <mherman@csg.org>
2022-12-12 10:23:40 -0500

    update to use recently released ncrp data for prison pop through 2020

--- R/05-get-state-prison-pop-ncrp.R
+++ R/05-get-state-prison-pop-ncrp.R
@@ -8,12 +8,12 @@ library(csgjcr)
 sp_path <- csg_sp_path("ad_hoc_requests/state_violent_crime_marshall/data")
 
 # load ncrp data, ds0004 (year-end population)
-# https://www.icpsr.umich.edu/web/NACJD/studies/38048
-load(file.path(sp_path, "ICPSR_38048-V1/ICPSR_38048/DS0004/38048-0004-Data.rda"))
+# https://www.icpsr.umich.edu/web/NACJD/studies/38492
+load(file.path(sp_path, "ICPSR_38492/DS0004/38492-0004-Data.rda"))
 
 # only 2010 and more recent and sentences greater than a year
 # TODO: check with jess if correct to limit to 1+ year sentences
-ncrp <- da38048.0004 |>
+ncrp <- da38492.0004 |>
   as_tibble() |>
   filter(RPTYEAR >= 2010, !SENTLGTH %in% "(0) < 1 year") |>
   mutate(
@@ -49,7 +49,7 @@ viol_prison_pop_by_off_state <- ncrp |>
 
 # write prison pop to disk
 write_rds(viol_prison_pop_state, file.path(sp_path, "viol_prison_pop_state.rds"))
-write_rds(viol_prison_pop_by_off_state, file.paht(sp_path, "viol_prison_pop_by_off_state.rds"))
+write_rds(viol_prison_pop_by_off_state, file.path(sp_path, "viol_prison_pop_by_off_state.rds"))
 
 
 
