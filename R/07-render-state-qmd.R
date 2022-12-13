d602ddd1ccd505aa94ccbd2d20a76a3340e93a3a
Matt Herman <mherman@csg.org>
2022-12-12 21:52:33 -0500

    add home page; update js deps loc to libs folder

--- R/07-render-state-qmd.R
+++ R/07-render-state-qmd.R
@@ -8,43 +8,18 @@ render_state <- function(state) {
     execute_params = list(state = state),
     output_file = paste0("state-viol-crime-", tolower(state), ".html"),
     quiet = TRUE
-
     )
 }
 
 # iterate over states and render
 purrr::walk(c(state.abb, "DC"), render_state)
 
-# define function to copy all html files to _site folder
-copy_html_to_site <- function(state) {
-  file.copy(
-    from = paste0("state-viol-crime-", tolower(state), ".html"),
-    to = paste0("_site/state-viol-crime-", tolower(state), ".html"),
-    overwrite = TRUE
-    )
-  }
-
-# iterate over states and copy
-purrr::walk(c(state.abb, "DC"), copy_html_to_site)
-
-# define function to delete html files in home dir
-delete_html <- function(state) {
-  unlink(paste0("state-viol-crime-", tolower(state), ".html"))
-}
-
-# iterate over states and delete
-purrr::walk(c(state.abb, "DC"), delete_html)
-
-# make sure to copy folder with js deps to _site
-file.copy(
-  from = "state-viol-crime_files/",
-  to = "_site/",
-  recursive = TRUE,
-  overwrite = TRUE
+# us page
+quarto::quarto_render(
+  input = "state-viol-crime-home.qmd",
+  output_file = "index.html",
+  quiet = TRUE
 )
-
-# delete js _deps
-unlink("state-viol-crime_files/", recursive = TRUE)
 
 # copy csg logo to site folder (prob is already there)
 file.copy(
@@ -67,3 +42,50 @@ file.copy(
   recursive = TRUE,
   overwrite = TRUE
 )
+
+# file.copy(
+#   from = "index.html",
+#   to = "_site/index.html",
+#   overwrite = TRUE
+# )
+#
+# file.copy(
+#   from = "state-viol-crime-home_files/",
+#   to = "_site/",
+#   recursive = TRUE,
+#   overwrite = TRUE
+# )
+#
+# unlink("index.html")
+# unlink("state-viol-crime-home_files/", recursive = TRUE)
+
+# # define function to copy all html files to _site folder
+# copy_html_to_site <- function(state) {
+#   file.copy(
+#     from = paste0("state-viol-crime-", tolower(state), ".html"),
+#     to = paste0("_site/state-viol-crime-", tolower(state), ".html"),
+#     overwrite = TRUE
+#     )
+#   }
+#
+# # iterate over states and copy
+# purrr::walk(c(state.abb, "DC"), copy_html_to_site)
+#
+# # define function to delete html files in home dir
+# delete_html <- function(state) {
+#   unlink(paste0("state-viol-crime-", tolower(state), ".html"))
+# }
+#
+# # iterate over states and delete
+# purrr::walk(c(state.abb, "DC"), delete_html)
+#
+# # make sure to copy folder with js deps to _site
+# file.copy(
+#   from = "state-viol-crime_files/",
+#   to = "_site/",
+#   recursive = TRUE,
+#   overwrite = TRUE
+# )
+#
+# # delete js _deps
+# unlink("state-viol-crime_files/", recursive = TRUE)
